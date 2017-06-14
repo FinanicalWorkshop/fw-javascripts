e0d9efe7a6932f0b11cd8de3ce30b0d5849ae4c7
delong <w.del@qq.com>
2017-06-14 20:24:56 +0800

    add new functions in Browser

--- core/browser-factory.js
+++ core/browser-factory.js
@@ -6,25 +6,40 @@ class BrowserFactory {
     get inApp() {
         return !!this.ua.match(this.app_stamp)
     }
+    
+    get inFXHApp(){
+        return !!this.ua.match('Easyloan888')
+    }
+
+    get inJRGCApp(){
+        return !!this.ua.match('FinancialWorkshop')
+    }
+
     get appVersion() {
         let r = this.ua.match(RegExp(this.app_stamp + '/(\\d+.\\d+.\\d+)'))
         return r ? r[1] : '0'
     }
+
     get inAndroid() {
         return /Android/i.test(this.ua)
     }
+
     get inIOS() {
         return /iPhone|iPad|iPod/i.test(this.ua)
     }
+
     get inMobile() {
         return this.inAndroid || this.inIOS
     }
+
     get inIOSApp() {
         return this.inApp && this.inIOS
     }
+
     get inAndroidApp() {
         return this.inApp && this.inAndroid
     }
+
     get inWeixin() {
         return /MicroMessenger/.test(this.ua)
     }
