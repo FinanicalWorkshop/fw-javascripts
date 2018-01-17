3fbeb709547e7e066ad22565baff3ea5e6d56f9d
DeronW <w.del@qq.com>
2018-01-17 13:48:15 +0800

    I'm too lazy, nothing left

--- core/utils.js
+++ core/utils.js
@@ -3,7 +3,7 @@ function cookie() {
     if (c === '') return r
     c.split(';').forEach((kv) => {
         let t = kv.trim().split('=');
-        r[t[0]] = t[1];
+        if(t) r[t[0]] = t[1];
     });
     return r;
 }
